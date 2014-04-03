7a3e12935746a93ebdbdf7ff7ad75297b40984d2
Mike Delaney <mdelaney@utexas.edu>
2014-04-02 19:31:49 -0500

    Change list items to checkboxes; add remove task function

--- myscript.js
+++ myscript.js
@@ -11,9 +11,38 @@ var addTask = function() {
 		document.getElementById("new_task").value="";
 		// create new list item
 		var li = document.createElement("li");
+		// create new label
+		var label = document.createElement("label");
+		label.className = "lbl";
+		// create new checkbox
+		var checkbox = document.createElement("input");
+		checkbox.className = "cb";
+		checkbox.type = "checkbox";
+		checkbox.name = "tasks";
+		checkbox.value = "tasks";
+		checkbox.addEventListener("click", removeTask, false);
+		// append checkbox to label
+		label.appendChild(checkbox);
 		// append text value from input
-		li.appendChild(document.createTextNode(new_task));
+		label.appendChild(document.createTextNode(new_task));
+		// append to list item
+		li.appendChild(label);
 		// insert in front of task list
-		task_list.insertBefore(li, firstChild);
+		task_list.insertBefore(li, firstChild);		
+	}
+}
+
+var removeTask = function() {
+	// get all checkboxes and labels
+	var boxes = document.getElementsByClassName("cb");
+	var labels = document.getElementsByClassName("lbl");
+	// iterate through items, remove clicked elements
+	for (var i=0; i<boxes.length; i++) {
+		box = boxes[i];
+		label = labels[i];
+		if (box.checked) {
+			box.parentNode.removeChild(box);
+			label.parentNode.removeChild(label);
+		}
 	}
 }
